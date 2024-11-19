41b46132f25b4f72d71ff7c3426e65d56c219b7a
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-11-19 19:45:22 +0300

    Complete task 3

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,8 @@
-/*добавьте сюда запрос для решения задания 3*/
+/*Top 3 restaurants where managers change most often.*/
+SELECT r.name AS restaurant_name, 
+       COUNT(*) AS amount_of_managers
+FROM cafe.restaurant_manager_work_dates rmwd
+LEFT JOIN cafe.restaurants r USING (restaurant_uuid)
+GROUP BY r.name
+ORDER BY COUNT(*) DESC
+LIMIT 3;
