9d7d837dd543639cdfc39f5472bc7109997671c6
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-11-19 19:48:33 +0300

    Complete task 4

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,14 @@
-/*добавьте сюда запрос для решения задания 4*/
+/*The pizzeria with the largest number of pizzas on the menu.*/
+WITH count_rank AS (
+    SELECT p.name AS name, 
+           COUNT(p.pizza_name) AS count, 
+           DENSE_RANK() OVER (ORDER BY COUNT(p.pizza_name) DESC) AS rank
+    FROM (SELECT name, 
+                 JSONB_EACH_TEXT(menu -> 'Пицца') AS pizza_name
+          FROM cafe.restaurants r 
+          WHERE r.type = 'pizzeria'::cafe.restaurant_type) AS p
+    GROUP BY p.name
+)
+SELECT name, count
+FROM count_rank
+WHERE rank = 1;
